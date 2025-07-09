4d82e4a97477816f67ba3ce35021e5d5b43b3eb8
Zeeshan Khan <126192035+Zeeshanprojects@users.noreply.github.com>
2025-07-09 01:11:26 -0700

    code updated

--- gotolive/src/assets/Images/Image.js
+++ gotolive/src/assets/Images/Image.js
@@ -7,5 +7,8 @@ const Images = {
   slider5: new URL("./slider5.webp", import.meta.url).href,
   slider6: new URL("./slider6.webp", import.meta.url).href,
   slider7: new URL("./slider7.webp", import.meta.url).href,
+  card1: new URL("./card1.webp", import.meta.url).href,
+  card2: new URL("./card2.webp", import.meta.url).href,
+  card3: new URL("./card3.webp", import.meta.url).href,
 };
 export default Images;
