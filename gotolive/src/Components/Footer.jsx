e5db748fa83668c45a344877e112e6e1dcc0c37c
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-09-17 11:34:17 +0500

    code updated

--- gotolive/src/Components/Footer.jsx
+++ gotolive/src/Components/Footer.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import Images from '../assets/Images/Image'
-import './Footer.css'
+import React from "react";
+import Images from "../assets/Images/Image";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-  <>
-  <div className='container  bg-transparent' >
-    <div className='row p-5'>
-        <div className='col-sm-12 col-md-12 col-lg-6'>
-            <img src={Images.logo} alt='footerlogo' className='footerlogo'/>
+    <>
+    <hr className="text-white"/>
+      <footer className="footer  text-light">
+        <div className="container py-5">
+          <div className="row align-items-center">
+            {/* Logo Section */}
+            <div className="col-sm-12 col-lg-6 text-center text-lg-start mb-4 mb-lg-0">
+              <img
+                src={Images.logo}
+                alt="footerlogo"
+                className="footerlogo mb-3"
+              />
+              <p className="small  mb-0">
+                © {new Date().getFullYear()} PTX. All Rights Reserved.
+              </p>
+            </div>
+
+            {/* Social Section */}
+            <div className="col-sm-12 col-lg-6 text-center text-lg-end">
+              <h5 className="mb-3">Get Social</h5>
+              <div className="d-flex justify-content-center justify-content-lg-end gap-3">
+                <a href="#" className="text-light fs-4">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="text-light fs-4">
+                  <i className="fab fa-linkedin"></i>
+                </a>
+                <a href="#" className="text-light fs-4">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="text-light fs-4">
+                  <i className="fab fa-youtube"></i>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-         <div className='col-sm-12 col-md-12 col-lg-6'>
-            <h5 className='text-white'> GET SOCIAL</h5>
-           
-        </div>
-    </div>
-  </div>
-  </>
-  )
+      </footer>
+    </>
+  );
 }
